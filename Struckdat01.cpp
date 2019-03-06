455522037b44520dd2fac28aab6d6af07ee6b67d
CukupUL <48039428+CukupUL@users.noreply.github.com>
2019-03-06 10:46:24 +0700

    PreTest-02

---
+++ Struckdat01.cpp
@@ -0,0 +1,30 @@
+/*
+ Nama nurul maarif
+ NPM 140810180040
+ judul 
+*/
+
+#include<iostream>
+using namespace std;
+
+void moveZeroToFront(int arr[],int n){
+	
+	}
+void input(int (&arr)[100], int& n){
+	
+	}
+void output(int arr[], int n){
+	
+	}
+
+int main(){
+	int arr[100];
+	int n;
+	
+	cout<<"masukan panjang data = "; cin>>n;
+	cout<<"masukan data [1 2 3 4 5] : " ;cin>>n;
+	input(arr,n);
+	
+	moveZeroToFront(arr,n);
+	output(arr, n);
+}
